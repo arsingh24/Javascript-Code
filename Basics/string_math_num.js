536b6bddc6b52fbea9468fcf51c057efbf23d9a1
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-02 17:54:52 +0530

    added a new feature some files changed

---
+++ Basics/string_math_num.js
@@ -0,0 +1,78 @@
+// let name= "Abhishek"
+// const company ="Codebucket"
+
+// console.log( `this is ${name} and I am a Intern at ${company} which is a company of the IT and deals with the `);
+
+//   const gameName = new String ('abhishek')
+
+//   console.log(gameName[0])
+//   console.log(gameName.__proto__)
+//   console.log(gameName.toUpperCase())
+//   console.log(gameName.charAt(4))
+//   console.log(gameName.indexOf('h'))
+//   console.log(gameName.lastIndexOf) 
+
+// let newString = gameName.substring(1,4)
+// console.log(newString)
+
+// let newString1 = gameName.slice(-7,2)
+// console.log(newString1)
+ 
+// let newString = gameName.substring(1,4)
+// console.log(newString)
+
+// let newString = gameName.substring(1,4)
+// console.log(newString)
+
+
+/*******************************Newchapter*****************************************/
+/*Number and maths */
+  
+// const score = 400
+// const marks = new Number(400);
+
+// console.log(score);
+// console.log(marks);
+
+// let i= score.toString()
+// console.log(score.toString())
+
+// console.log(i.slice(2));
+
+// console.log(score.toFixed(4));
+
+// const hundreds = 10000000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/*+++++++++++++++++++----------Maths--------------------++++++++ */
+
+const hundreds = 1000000
+// to generate a round off value
+console.log(Math.round(4.6)) 
+
+// to generate a floor value i.e. 4
+console.log(Math.floor(4.6))
+
+// to generate a ceiling value i.e. 5
+console.log(Math.ceil(4.6))
+
+// to generate a random number 
+console.log(Math.random())
+
+// to generate a random number *10 
+console.log(Math.random()*10)
+
+// to generate a random number that will be greater than 1
+console.log((Math.random()*10)+1)
+
+// to generate a random number to get a number that will greater than 1 and is floor value
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max= 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+// to generate a random number get a number tht will be greater than min value
+
+
+
+
