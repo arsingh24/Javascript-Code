546e535cd063fba5f45e56ec9e591566f6837d07
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-05 17:48:45 +0530

    Basic of Object Part-1 end

---
+++ Object/obj1.js
@@ -0,0 +1,32 @@
+
+// const mySum = Symbol()
+// const JsUser = {
+//     name : "abhishek",
+//     age : 24,
+//     // symbol should be written in this format only in obj
+//     [mySum]: "keyyy1",
+//     location: "Patna",
+//     email: "arsingh4994@gmail.com",
+//     isLoggedIn: false,
+//     Today : Date(),
+// }
+
+// JsUser.location="Dehradun"
+// two ways to access Object entity
+// // 1st way
+// console.log(JsUser.name)
+// // 2nd Way
+// console.log(JsUser["location"])
+
+// to keep the data unchanged use Object.freeze ()
+// Object.freeze (JsUser)
+// console.log(JsUser.age ="30",JsUser )
+
+// JsUser.greeting = function(){
+//     console.log(("hello hii choro jai shri Ram bolo").toUpperCase())
+// }
+// console.log(JsUser.greeting)
+// console.log(JsUser.greeting())
+
+
+// +++++++++++++++----------------Part-2------++++++++++++++++++++++++++++++++++++++++++++++++++
