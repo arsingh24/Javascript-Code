2d5d82be567d991c2abe0616f6886e1c1cbc6bb6
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-07 18:24:13 +0530

    Basic of function

---
+++ Function/fun1.js
@@ -0,0 +1,75 @@
+// function sayMyName(){
+//     const userName ={
+//         name : "Abhishek",
+//         lastNme: "Singh"
+//     }
+//     console.log(`the name of the user is ${userName.name+" "+userName.lastNme}`);
+// }
+// sayMyName()
+
+// function sayMyName(name, lastName) {
+//     console.log(`The name of the user is ${name} ${lastName}`);
+// }
+// sayMyName("Amitabh", "Bachchan");
+
+
+// function addTwoNumbers(number1,number2){
+    
+//     console.log(number1+number2);
+// }
+
+// addTwoNumbers(4,8)
+
+// function addTwoNumbers1(number1,number2){
+    
+//     console.log(number1+number2);
+// }
+
+// const result =addTwoNumbers1(4,8)
+
+
+// function addTwoNumbers2(number1,number2){
+    
+//     // console.log(number1+number2);
+//         return number1+number2;
+// }
+
+// const result2=addTwoNumbers2(4,8)
+// console.log(result2);
+
+
+// function loginUserMessage(userName){
+//     if (userName===undefined){
+//         console.log("nam nhi hai")
+//         return;
+//     }
+//     return`${userName} just logged in`
+// }
+
+// console.log(loginUserMessage())
+
+
+// passing any num of value in function
+// function calculateCarPrice(...num1){
+//     return num1;
+// }
+// console.log(calculateCarPrice(400,500,1000))
+
+
+// const user ={
+//     username: "Abhishek",
+//     age: 24,
+// }
+// // Passing object in function
+// function Object1(anyobject){
+//     console.log(`username isn ${anyobject.username}`)
+// }
+// Object1(user)
+// console.log()
+
+// // passing array in function
+// let arr1=[200,4999,5000];
+// function arr3(arraylao){
+//     console.log(arraylao);
+// }
+// arr3(arr1)
