5ea2626f473ea0285e9bca3cf8092f24dfd83756
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 17:25:02 +0530

    for each loop use in array

---
+++ Loops/for_each.js
@@ -0,0 +1,45 @@
+const coding =["js", "ruby","cpp","c"]
+
+// coding.forEach( function (item)
+//     {
+//      console.log(item);    
+// })
+
+// coding.forEach( (item)=>
+//     {
+//      console.log(item);    
+// })
+
+// function printArr (item)
+// {
+//     console.log(item);    
+// }
+// coding.forEach(printArr)
+
+
+//by this we can access the item index and the whole array 
+// coding.forEach((item, index, arr)=>{
+//     console.log(item , index, arr)
+// })
+
+
+const myCoding =[
+    {
+        languageName: "javascript",
+        languageFileName: "js" 
+    },
+
+    {
+        languageName: "C++",
+        languageFileName: "cpp" 
+    },
+
+    {
+        languageName: "java",
+        languageFileName: "java" 
+    }
+]
+    
+myCoding.forEach((item)=>{
+     console.log(item.languageFileName)
+})
