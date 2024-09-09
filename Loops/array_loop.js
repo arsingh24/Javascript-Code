c4451c25f0300a797679748099693ae28eac4360
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 17:18:59 +0530

    for of loop in array

---
+++ Loops/array_loop.js
@@ -0,0 +1,47 @@
+
+
+// =========for(let key of array)================
+
+// const arr=[1,2,3,4,5,6];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings= "Hello World!"
+
+// for (const greet of greetings) {
+//     if (greet ==" ") {
+//         console.log("  ")
+//         continue 
+//     }
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+
+
+// const map =new Map()
+// map.set ('IN', "India")
+// map.set ('US', "United States of America")
+// map.set ('Fr', "France")
+// map.set ('IN', "India")
+
+// // console.log(map);
+
+// for (const [key,value] of map) {
+//     // console.log(`${key} :- ${value}`);
+// }
+
+
+// for of is not used in object
+// const myObj ={
+//     game1:"COD",
+//     game2:"PUBg",
+    
+// }
+// // console.log(myObj);
+
+// for (const game for myObj) {
+//     console.log(game);
+// }
