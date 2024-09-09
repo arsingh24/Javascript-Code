3e39eefaf09a4276ff1c6a6ab69d33f4b5b41564
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 13:23:51 +0530

    this and arrow function

---
+++ Control_Flow/one.js
@@ -0,0 +1,59 @@
+// All the Conditional Operator and its useage
+
+// < (less than) , >(greater than) , <=(less than equal to)  , >=(greater than equal to)
+// = (to assign value ) , ==(to check equal or not) , != (not equal to)
+// === (checks the typeof of the both values )
+
+
+// ============if /else condition=================
+// const val1 = 2;
+// const val2= "2";
+
+// if (val1===val2){
+//     console.log("u r my type");
+// }
+// else{
+//     console.log("u r not my type")
+// }
+
+// // if can be defined as it also
+// const bal=1000;
+// if (bal>100) console.log("Paisa h bhau")
+
+
+// // =======if else if condition=======================
+// const balance =1000;
+//     if (balance < 500) {
+//     console.log("Qauter ya half bakadi aa jayega")
+//     } 
+//     else if (balance < 750) {
+//     console.log("Half aa jayega")
+//     }
+//      else if (balance<900) {
+//         console.log("ek full aa jayega");    
+//     }
+//     else {
+//         console.log("ek Blender Pride aa jayega")
+//     }
+
+
+// ==========conditions statements=============
+// &&  and condition
+// const userLoggedIn = true
+// const debitCard = true
+// if (userLoggedIn && debitCard && 2==2 ) {
+// console.log("Allow to buy course")
+// }
+
+//  || or condition
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard && 2==2 ) {
+// console.log("Allow to buy course")
+// }
+// if (loggedInFromGoogle|| loggedInFromEmail){
+//     console.log("User logged in")
+// }
