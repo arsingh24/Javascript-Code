63fca51faea057328d373301bf4b140e2d65896f
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 15:00:14 +0530

    Truthy and falsy values

---
+++ Control_Flow/truthy.js
@@ -0,0 +1,33 @@
+// const userEmail="abhishek@ai";
+
+
+// if (userEmail){
+//     console.log("got email😁😁");
+// }
+// else{
+//     console.log("nhi milaaa🥺🥺");
+    
+// }
+
+
+// falsy values
+// all of these will return false as response
+// false , 0 ,-0 , BigInt 0n,"", "null", undefined, NaN
+
+
+// truthy values
+// all of these will return true as response
+// true , [] , "0" , 'false' , " ",{}, function(){}
+
+// const userEmail=[]
+// if (userEmail.length===0){
+//     console.log("Array is empty")
+// }
+
+// const emptyObj ={}
+// if (Object.keys(emptyObj).length===0){
+//     console.log("Object is array") 
+// }
+
+
+
