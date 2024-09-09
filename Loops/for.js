468c7aee8df9afad96056ff9adb5f072ee3db10f
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 17:16:15 +0530

    for loops

---
+++ Loops/for.js
@@ -0,0 +1,39 @@
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop =${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+//     console.log("\n")
+//   }
+
+
+
+// let myArmy =["flash","Batman","Superman"];
+
+// for (let index = 0; index <myArmy.length; index++) {
+//     const element = myArmy[index];
+//     console.log(`here are the ${element}`)
+    
+// }
+
+
+//======================= break in loop===============================
+// break is used to terminate the command after the condition is true
+// for (let index = 1; index <=20; index++) {
+//     if (index==5) {
+//         console.log(`Detected 5`)
+//         break
+//     }
+//     console.log(`Values of the index ${index}`)
+// }
+
+// ============Continue==========================
+// Continue is used to skip the condition only otherwise print all the element except the condition
+// for (let index = 1; index <=20; index++) {
+//     if (index==5) {
+//         console.log(`Detected 5`)
+//         continue
+//     }
+//     console.log(`Values of the index ${index}`)
+// }
