089d201baae5427df06a69505efe0e13e2daf90d
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 14:59:15 +0530

    Switch Case

---
+++ Control_Flow/switch.js
@@ -0,0 +1,25 @@
+// Switch Case 
+const month=7;
+
+switch (month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;    
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    default:
+        console.log("Kuch match nhi huaa")
+}
