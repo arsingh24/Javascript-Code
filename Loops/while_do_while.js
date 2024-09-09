154b1a4cf58729b5177006fa838a520867736c6e
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 17:16:58 +0530

    while do while loops

---
+++ Loops/while_do_while.js
@@ -0,0 +1,26 @@
+
+// let i= 0;
+// while (i<=10) {
+//     console.log(`values of index is ${i}`);
+//     i =i+2;
+// }
+
+// let myArmy =["flash","Batman","Superman"];
+// let iter=0;
+// while (iter<myArmy.length) {
+//     console.log(`Values is ${myArmy[iter]}`)
+//     iter=iter+1;
+    
+// }
+
+
+
+// ===========do while===============
+
+// let score =1;
+// do{
+//     console.log(`value of ${score}`);
+//     score=score+3;
+// }while(score<=10);
+
+
