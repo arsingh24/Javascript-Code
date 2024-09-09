22fe4f9860ffbe8c3c440d5269da5e4d41ca2a7c
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-09 14:58:19 +0530

    Nullish and Terniary Operator

--- Control_Flow/one.js
+++ Control_Flow/one.js
@@ -57,3 +57,23 @@
 // if (loggedInFromGoogle|| loggedInFromEmail){
 //     console.log("User logged in")
 // }
+
+
+// ============================================================================
+// Nullish Coalescing Operator(??): null undefined
+// it is used to return the value instead of null or undefined
+
+// let val1;
+// // val1 = 5?? 10;
+// // val1 = null?? 10;
+// // val1 = undefined?? 10;
+// val1 = undefined?? null;
+// console.log(val1)
+
+//  Terniary Operator
+
+// condition ? true:false
+
+
+const iceTeaPrice =100
+ iceTeaPrice <=80 ? console.log("less than 80"):console.log("more than 80")
