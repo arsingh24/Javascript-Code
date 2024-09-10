40eee36b33884e8c613f6d9f4b0ff8701c7383ab
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-10 22:42:42 +0530

    filter() use and implementation in array

---
+++ Loops/filter.js
@@ -0,0 +1,49 @@
+// const myNum=[1,2,3,4,5,6,7,8,9,10];
+
+// // it will return the output 
+// const newNum =myNum.filter((num)=>num >=4)
+// console.log(newNum)
+
+// // we have to use return to get the output when we use { } after the => function
+// const newNum1=myNum.filter((num)=>{
+//    return num >=4
+// }
+// )
+// console.log(newNum1);
+
+// const newNum2 = []
+// myNum.forEach((num)=>{
+//     if (num>4){
+//         newNum2.push(num)
+//     }
+// })
+
+// console.log(newNum2)
+
+
+const books=[
+    { title: 'Book One', genre: 'Fiction', publish: 1981,
+    edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', Ipublish: 1992,
+    edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999,
+    edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989,
+    edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009,
+    edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987,
+    edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986,
+    edition: 1996 },
+];
+
+
+// by this we can access the items of the obejct of the array
+// const userBook = books.filter((bk)=>bk.edition>=2000);
+// this is used only for counting the number how many outcomes are coming
+// {const count = userBook.length;
+// console.log(count)}
+
+// // console.log(userBook);
+
