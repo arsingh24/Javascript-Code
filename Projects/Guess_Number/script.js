985f487568892dcedc1169f545f2418dea829513
Abhishek Raj Singh <99174875+arsingh24@users.noreply.github.com>
2024-09-13 18:07:07 +0530

    html of BMI calculator

---
+++ Projects/Guess_Number/script.js
@@ -0,0 +1,55 @@
+const randomNumber=parseInt(Math.random()*100+1);
+
+const submit =document.querySelector('#subt');
+const userInput =document.querySelector('#guessField');
+const guessSlot =document.querySelector('.guesses');
+const remaining =document.querySelector('#lastResult');
+const lowOrHigh =document.querySelector('#lowOrHi');
+const startOver =document.querySelector('#resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess =[];
+let numGuess = 1;
+
+let playGame = true;
+if(playGame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess= parseInt(userInput.value);
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+ if(isNaN(guess)){
+    alert('Please enter a valid number')
+ }else if (guess<1){
+    alert('Please enter a number more than 1')
+ }else if (guess>1){
+    alert('Please enter a number less than 100')
+ }
+ else{
+    prevGuess.push(guess)
+ }
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function newGame(){
+
+}
+
+function endGame(){
+
+}
---
+++ SmallApp/pratice.js
@@ -0,0 +1,5 @@
+const arr=[1,2,3];
+    console.log(arr);
+    let a1 = Math.floor(arr.length/2);
+    console.log(a1);
+    console.log(arr[a1]);
